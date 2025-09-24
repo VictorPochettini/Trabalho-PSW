ca345ea860da2036a55ba55b4b19fd0c36a66e30
mlvSantiago <marialuisasantiagoccp@gmail.com>
2025-09-24 11:37:40 -0300

    Adicionando js na pag de criação de conta

---
+++ js/criacaoConta.js
@@ -0,0 +1,12 @@
+const icone = document.getElementById("vizualizacao");
+const senha = document.getElementById("senha");
+
+icone.addEventListener("click", ()=>{
+    if(senha.type=="password"){
+        senha.type="text";
+        icone.querySelector("img").src = "images/olhoAbertoRoxo.png";
+    }else{
+        senha.type = "password";
+        icone.querySelector("img").src="images/olhoFechadoRoxo.png";
+    }
+});
