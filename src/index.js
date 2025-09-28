849b0f24b9c232f1bcd0f0f3a4d1a8c4b104018d
JoaoCPM10 <joaopmello2@gmail.com>
2025-09-28 22:44:34 +0000

    Otimização OK

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Importe seu CSS global
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importe o CSS do Bootstrap
-import App from './App_';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
